c55f000b2777de97c29bcd9c19103d9eba22b878
Shahrad Elahi <shahradq@gmail.com>
2022-11-28 19:58:41 +0330

    Updated the dependencies #2

--- src/App.php
+++ src/App.php
@@ -2,7 +2,6 @@
 
 namespace ShahradElahi\DurgerKing;
 
-use EasyHttp\Client;
 use TelegramBot\Entities\Update;
 use TelegramBot\Request;
 use TelegramBot\Telegram;
@@ -16,33 +15,30 @@ use TelegramBot\Telegram;
  *
  * @link https://core.telegram.org/bots/api#getting-updates
  */
-class App extends \TelegramBot\UpdateHandler
-{
+class App extends \TelegramBot\UpdateHandler {
 
-    /**
-     * This method is called when the bot receives a new message.
-     *
-     * @param Update $update
-     * @return void
-     */
-    public function __process(Update $update): void
-    {
+   /**
+    * This method is called when the bot receives a new message.
+    *
+    * @param Update $update
+    * @return void
+    */
+   public function __process(Update $update): void {
 
+      Telegram::setAdminId($_ENV['ADMIN_CHAT_ID']);
 
-//        Telegram::setAdminId($_ENV['ADMIN_CHAT_ID']);
+      if ($update->getMessage()->getText() === '/ping') {
+         Request::sendMessage([
+            'chat_id' => $update->getMessage()->getChat()->getId(),
+            'parse_mode' => 'Markdown',
+            'text' => '`Pong!`',
+         ]);
+      }
 
-        if ($update->getMessage()->getText() === '/ping') {
-            Request::sendMessage([
-                'chat_id' => $update->getMessage()->getChat()->getId(),
-                'parse_mode' => 'Markdown',
-                'text' => '`Pong!`',
-            ]);
-        }
-
-        self::addPlugins([
-            Plugins\Commands::class,
-            Plugins\WebService::class,
-        ]);
-    }
+      self::addPlugins([
+         Plugins\Commands::class,
+         Plugins\WebService::class,
+      ]);
+   }
 
 }
